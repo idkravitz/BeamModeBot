57694a2365104f94ad49b756083c3d483272b0bf
idkravitz <idkravitz@gmail.com>
2025-12-08 01:48:18 +1000

    Implement sane help msg

--- src/beammodebot/main.py
+++ src/beammodebot/main.py
@@ -20,6 +20,15 @@ async def _main_async():
     async with TelegramClient('bot', api_id, api_hash) as bot:
         await bot.start(bot_token=BOT_TOKEN)
 
+
+        @bot.on(events.NewMessage(pattern='/start'))
+        async def start_handler(event: events.NewMessage.Event):
+            """Show help message, but only in direct messages."""
+            await event.reply("""Hello!
+      
+To use this bot, simply type "@BeamModeBot " into your text box and click one of the results or click the button attached to this message.""",
+buttons=[[Button.switch_inline('Share your beam mode!', query='')]])
+
         @bot.on(events.InlineQuery)
         async def handler(event: events.InlineQuery.Event):
             builder = event.builder
@@ -45,7 +54,7 @@ async def _main_async():
                 ),
                 builder.article(
                     title='HALP!',
-                    text="Press the 1st (top) button, you retard",
+                    text="Either press the button attached to this message and select the chat you would like to post in or simply enter \"@BeamModeBot \" into your text box.",
                     description="Send the usage guideliness to this chat.",
                     thumb = thumb,
                     buttons=buttons
