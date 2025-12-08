ece52d84b7950be4a2a6b194ed8f76e28a3c525c
idkravitz <idkravitz@gmail.com>
2025-12-09 00:48:29 +1000

    Merged roll funcs in one

--- src/beammodebot/pseudorand.py
+++ src/beammodebot/pseudorand.py
@@ -1,17 +1,9 @@
 import mmh3
 
-def roll_100_for_string(s: str, unix_epoch: int) -> int:
+def roll100(key: str | int, unix_epoch: int) -> int:
     """
     Produces non crypto safe shitty presudorandom number in range [0, 100]
     """
     epoch_m = unix_epoch // 60
-    hash = mmh3.mmh3_x64_128_uintdigest(f'{s}|{epoch_m}'.encode('utf-8'))
-    return hash % 101
-
-def roll_100_for_user(user_id: int, unix_epoch: int) -> int:
-    """
-    Produces non crypto safe shitty presudorandom number in range [0, 100]
-    """
-    epoch_m = unix_epoch // 60
-    hash = mmh3.mmh3_x64_128_uintdigest(f'{user_id}|{epoch_m}'.encode('utf-8'))
+    hash = mmh3.mmh3_x64_128_uintdigest(f'{key}|{epoch_m}'.encode('utf-8'))
     return hash % 101
